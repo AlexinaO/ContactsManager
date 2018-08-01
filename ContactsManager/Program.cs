1319a0ec43de0d20b283c55722d28b383aa1fca4
AlexinaO <alexina.orsoni@gmail.com>
2018-08-01 17:32:39 +0200

    code pour contacts manager

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -8,8 +8,89 @@ namespace ContactsManager
 {
     class Program
     {
+        static List<string> contacts = new List<string>();
+
+
         static void Main(string[] args)
         {
+            Console.Clear();
+            AfficherMenu();
+            string choix;
+            do
+            {
+                choix = Console.ReadLine();
+                switch (choix)
+                {
+                    case "1":
+                        Console.Clear();
+                        ListerContacts();
+                        AfficherMenu();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        AjouterContacts(contacts);
+                        AfficherMenu();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        SupprimerContact();
+                        AfficherMenu();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Quitter();
+                        AfficherMenu();
+                        break;
+                    default:
+                        Console.WriteLine("L'application va fermer");
+                        break;
+                }
+            } while (choix != "4");
+
+            Console.ReadKey();
         }
+     
+        static void AfficherMenu()
+        {
+            string[] menu = new string[4] { "1.Liste des contacts", "2.Ajout d'un contact", "3.Suppression d'un contact", "4.Quitter" };
+            Console.WriteLine("CONTACTS MANAGER");
+            Console.WriteLine("Menu");
+            Console.WriteLine(menu[0]);
+            Console.WriteLine(menu[1]);
+            Console.WriteLine(menu[2]);
+            Console.WriteLine(menu[3]);
+            Console.WriteLine("\nFaites votre choix:");
+       
+
+        }
+        static void ListerContacts()
+        {
+            Console.WriteLine("Voici la liste des contacts");
+           
+            foreach (var element in contacts)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        static void AjouterContacts(List<string>contacts)
+        {
+            Console.WriteLine("Ajout d'un contact");
+            Console.WriteLine("Entrez le nom du contact:");
+            contacts.Add(Console.ReadLine());
+            Console.WriteLine("Entrez le prenom du contact:");
+            contacts.Add(Console.ReadLine());
+        }
+        static void SupprimerContact()
+        {
+            Console.WriteLine("Supprimer un contact");
+            Console.WriteLine("Entrez le nom du contact à supprimer:");
+            contacts.Remove(Console.ReadLine());
+
+        }
+        static void Quitter()
+        {
+            Environment.Exit(0);
+        }
+       
     }
 }
